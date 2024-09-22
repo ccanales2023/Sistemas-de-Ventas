afa8e635cea0b170d3549e76fb555ce17f58b6b4
ccanales2023 <orion.1781@gmail.com>
2024-09-21 22:02:04 -0500

    creacion de proyecto

---
+++ src/test/java/com/portafolio/Sistemas/de/Ventas/SistemasDeVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.Sistemas.de.Ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemasDeVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
